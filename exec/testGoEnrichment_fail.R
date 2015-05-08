b21d29bd0d7e193092edb9afa3c9ccfaf147be6c
Dr. Asis Hallab <hallab@mpipz.mpg.de>
2015-05-08 18:41:24 +0200

    Added faster running tests

--- exec/testGoEnrichment_fail.R
+++ exec/testGoEnrichment_fail.R
@@ -1,40 +1,11 @@
-################################################
-# Example input file to R package goEnrichment #
-################################################
-
-# require(GOstats)
-# require(GSEABase)
-# require(RMySQL)
-# source( "../R/goEnrichment.R" )
 require(goEnrichment)
 
-#' Define background gene set collection
-#' [package: GSEABase] - To define your own see [1]
-#' See data( package='goEnrichment' ) - every object terminating in '.gsc'
-#' can be used.
-# load( "~/R_libs/goEnrichment/data/ath_gene_set_collection.RData" )
-data( "ath_gene_set_collection", package="goEnrichment" )
-gsc <- ath.gsc # Default is TAIR10 GO annotations
+data("ath_go_test", package = "goEnrichment")
+data("ath_gene_set_collection_test", package = "goEnrichment")
 
-#' Define background GO annotations to compare foreground (hypothesis) GO
-#' annotations with:
-#' See data( package='goEnrichment' ) - every object terminating in '.go' can
-#' be used.
-# load( "~/R_libs/goEnrichment/data/ath_goa.RData" )
-data( "ath_goa", package="goEnrichment" )
-univ.gene.ids <- as.character(unique(ath.go$V3)[1:1000]) # Default is all TAIR10 GO annotated genes.
-
-#' Load foreground gene IDs, that it those genes for which to identify
-#' enriched GO terms. Of course these gene IDs need to be part of the above
-#' used Gene Ontolog Annotation table (e.g. 'ath.go').
-#' Put in something like the following line:
+univ.gene.ids <- as.character(unique(ath.go.tst$V3))
 gene.ids <- univ.gene.ids[1:2]
-
-#' Finally define a path to your output table:
 output.tbl <- "goEnrichment_test_output_fail.tsv"
-
-# Run analysis
-enrich.gos <- joinGOEnrichResults(goEnrichTest(gsc, gene.ids, univ.gene.ids, cond=TRUE))
-
-# Write output:
+enrich.gos <- joinGOEnrichResults(goEnrichTest(ath.gsc.tst, gene.ids, univ.gene.ids, 
+  cond = TRUE))
 write.table(enrich.gos, output.tbl, row.names = FALSE, quote = FALSE, sep = "\t") 
